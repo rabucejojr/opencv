fc3eecaa15143b7e8f965b51c694bd9a6ae49eb8
rabucejo <rogerhapay@gmail.com>
2022-04-25 08:05:34 +0800

    added new file syntax

--- index.py
+++ index.py
@@ -1,32 +1,25 @@
 import os
 import cv2
 
-cam = cv2.VideoCapture(0)
-
-cv2.namedWindow("test")
-
+# Take input of the person name
+name = input("Enter name:  ")
 img_counter = 0
+# Create the videocapture object
+cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
-    cv2.imshow("test", frame)
-
+    # Read each frame
+    success, frame = cap.read()
+    # Show the output
+    cv2.imshow("Frame", frame)
     k = cv2.waitKey(1)
-    if k % 256 == 27:
+    if k == ord('c'):
+        path = r'C:\Users\programmer\Desktop\OPENCV Python/images'
+        img_name = name+"{}.png".format(img_counter)
+        cv2.imwrite(os.path.join(path, img_name), frame)
+        print("Image Saved- ", img_name)
+        break
+    elif k % 256 == 27:
         # ESC pressed
         print("Escape hit, closing...")
         break
-    elif k % 256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        path = r'C:\Users\programmer\Desktop\OPENCV Python/images'
-        cv2.imwrite(os.path.join(path, img_name), frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
-
-cam.release()
-
-cv2.destroyAllWindows()
